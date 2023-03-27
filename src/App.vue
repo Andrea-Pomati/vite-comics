75696263b1a601e88326b46700f4612833c6607f
Andrea-Pomati <pomati.andrea@gmail.com>
2023-03-27 19:32:42 +0200

    feat: header, main and footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <script>
-
+import AppHeader from './components/AppHeader.vue';
+import AppBuy from './components/AppBuy.vue';
+import AppFooter from './components/AppFooter.vue';
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    AppHeader,
+    AppBuy,
+    AppFooter,
+  }
+}
 </script>
 
 <template>
+  
+  <AppHeader>
+
+    
+  </AppHeader>
+
+
+  <AppBuy>
+    
+  </AppBuy>
+
+
+  <AppFooter>
+
+  </AppFooter>
 
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+    name: "AppHeader",
+  data() {
+    return {
+      
+    }
+  }
+} 
+
+</script>
+
+
+<template>
+
+<header>
+
+    <div class="container flex-nav">
+        
+        <img src="/img/dc-logo.png" alt="">
+
+        <ul id="flex-li">
+            <li>CHARACHTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+        </ul>
+
+
+
+    </div>
+
+
+</header>
+
+
+
+</template>
+
+<style lang="scss" scoped>
+
+header{
+    background-color: white;
+    color: #6D6D6C;
+    font-weight: bold;
+
+
+}
+
+#flex-li{
+    display: flex;
+    gap: 10px;
+}
+
+.flex-nav{
+    display: flex ;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 0;
+
+}
+
+
+
+
+</style>
